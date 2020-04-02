7567ff5443c5f479bceef83698f750054cee7e6d
ant443 <inconspicuous443@gmail.com>
2020-04-02 19:44:30 +0100

    refactor

--- write_weeks_tasks.py
+++ write_weeks_tasks.py
@@ -17,24 +17,26 @@ def output_weeks_tasks_and_update_file():
                     L.append(line)
             return L
     
-    def read_weekday_tasks(file_location):
+    def convert_to_dict(weekday_tasks_list):
         weekdays = {"Monday": "", "Tuesday": "", "Wednesday": "", 
                 "Thursday": "", "Friday": "", "Saturday": "", "Sunday": ""}
         dayname = ""
         days_tasks = ""
         days_found = set([])
-        tasks_and_days_list = read_data(file_location)
-        for i in tasks_and_days_list:
+        for i in weekday_tasks_list:
             if i in weekdays.keys():
                 if dayname:
                     weekdays[dayname] = days_tasks
                     days_tasks = ""
                 dayname = i
+                if dayname in days_found:
+                    raise Exception("Duplicate day found on it's own line, in weekly tasks list.")
                 days_found.add(dayname)
             else:
                 days_tasks += i + "\n"
         weekdays[dayname] = days_tasks.rstrip("\n")
-        assert len(days_found) == 7
+        if len(days_found) != 7:
+            raise Exception("A day is missing from weekly tasks list. Make sure each day appears on it's own line")
         return weekdays
     
     def make_nested_lists(single_list):
@@ -54,13 +56,13 @@ def output_weeks_tasks_and_update_file():
                              "Expected digits then d or w. Example: 24w")
             
     def validate_format(tasks_by_date):
-        for i in range(len(tasks_by_date)):
-            validate_three_items(tasks_by_date[i])
-            validate_frequency(tasks_by_date[i][1])
+        for inner_list in tasks_by_date:
+            validate_three_items(inner_list)
+            validate_frequency(inner_list[1])
             
-    def convert_to_num(day):
+    def convert_to_num(dayname):
         import time
-        return time.strptime(day, "%A").tm_wday
+        return time.strptime(dayname, "%A").tm_wday
     
     def days_between_weekday(num1, num2):
         oneweek = 7
@@ -85,7 +87,7 @@ def output_weeks_tasks_and_update_file():
         def missed_task_msg(date_indicated, days_old):
             return ("old task detected. Should have been done on "
                 "{}({} days ago): ".format(date_indicated, days_old))
-            
+
         def make_timedelta(task_frequency):
             if task_frequency[-1] == 'd':
                 return datetime.timedelta(days=int(task_frequency[:-1]))
@@ -98,15 +100,15 @@ def output_weeks_tasks_and_update_file():
         
         def convert_to_string(date_obj):
             return date_obj.strftime('%b %d %Y')
-        
+
         tasks_by_date = tasks_by_date[:]
         weeks_tasks = weeks_tasks[:]
         
-        for i in range(len(tasks_by_date)):
-            task_date = tasks_by_date[i][0]
-            task_date_obj = convert_to_date_obj(tasks_by_date[i][0])      
-            task_frequency = tasks_by_date[i][1]
-            task_text = tasks_by_date[i][2]
+        for inner_list in tasks_by_date:
+            task_date = inner_list[0]
+            task_date_obj = convert_to_date_obj(inner_list[0])      
+            task_frequency = inner_list[1]
+            task_text = inner_list[2]
             
             if task_date_obj <= date_in_loop:
         # add task to weeks_tasks plus a notice if old date detected
@@ -117,13 +119,13 @@ def output_weeks_tasks_and_update_file():
         # update the tasks date        
                 New_date_obj = date_in_loop + make_timedelta(task_frequency)
                 New_date_str = convert_to_string(New_date_obj)
-                tasks_by_date[i][0] = New_date_str
+                inner_list[0] = New_date_str
         return tasks_by_date, weeks_tasks
     
     def replace_file_data(location, tasks_list):
         with open(location, 'w') as f:
-            for i in tasks_list:
-                f.write(", ".join(i) + '\n')
+            for inner_list in tasks_list:
+                f.write(", ".join(inner_list) + '\n')
                 
     def create_output_file(location, weeks_tasks):
         with open(location, 'a') as f:
@@ -134,7 +136,8 @@ def output_weeks_tasks_and_update_file():
     startday = "Monday"    
     tasks_by_date_location = "tasks_by_date.txt"
     weekday_tasks_location = "monday_to_sunday_tasks.txt"
-    tasks_by_weekday = read_weekday_tasks(weekday_tasks_location)
+    weekday_tasks_list = read_data(weekday_tasks_location)
+    tasks_by_weekday = convert_to_dict(weekday_tasks_list)
     tasks_by_date = read_formatted_data(tasks_by_date_location)
     validate_format(tasks_by_date)
     days_to_startday = days_between_weekday(todays_date.weekday(),
@@ -149,20 +152,13 @@ def output_weeks_tasks_and_update_file():
         weeks_tasks.append(readable_date + '\n')
         tasks_by_date, weeks_tasks = process_matching_dates(date_in_loop, 
                                                            tasks_by_date, 
-                                                           weeks_tasks)
+                                                           weeks_tasks)                                                 
         weeks_tasks.append(tasks_by_weekday.get(readable_date.split()[0])+'\n')
         
-    replace_file_data(tasks_by_date_location, tasks_by_date)
     output_location = "weeks_tasks.txt"
     create_output_file(output_location, weeks_tasks)
+    replace_file_data(tasks_by_date_location, tasks_by_date)
             
-    print("Task complete", end='')
+    print("Task complete")
 
 output_weeks_tasks_and_update_file()
-
-
-
-# TODO:
-# Add ability to use m and y in tasks_by_date.txt for months and years
-# Add ability for script to remove task if labeled not regular in some way, 
-# e.g. a one off appointment.
